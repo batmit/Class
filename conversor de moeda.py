003018c74c260207d376d278780ce807688ddca2
batmit <45146744+batmit@users.noreply.github.com>
2020-07-16 17:44:12 -0300

    Create conversor de moeda.py

---
+++ conversor de moeda.py
@@ -0,0 +1,8 @@
+n = float(input('Digite o valor do dólar: '))
+n2 = float(input('Digite o valor do euro: '))
+n3 = float(input('Digite o valor do Yen: '))
+n1 = float(input('Digite o valor em reáis: '))
+do = n1 / n
+eu = n1 / n2
+ye = n1 / n3
+print(f'Dólar: {do}, euro: {eu}, yen : {ye}.')
